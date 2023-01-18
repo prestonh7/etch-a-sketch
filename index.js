a76b0ef344499f264dd4d0402be676e0e6888a6d
Preston <PrestonHouse7@gmail.com>
2023-01-17 19:27:00 -0600

    Added eraser

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const drawScreen = document.querySelector('.drawScreen');
 let input = document.querySelector('input');
 let numSelection = input.value;
 const confirmSize = document.getElementById('confirm');
+const erase = document.getElementById('eraser');
+const pin = document.getElementById('pin')
+let draw = true;
+
+let box = document.getElementsByClassName('box')
 
 confirmSize.addEventListener('click', createGrid);
+erase.addEventListener('click', changeMode);
+pin.addEventListener('click', changeMode);
 
 function createGrid() {
     numSelection = input.value;
@@ -13,5 +20,14 @@ function createGrid() {
         const square = document.createElement('div');
         square.classList.add('box');
         drawScreen.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            if(draw) {square.style.backgroundColor = "black"}
+            else if(!draw) {square.style.backgroundColor = "#e5eaf5"}
+        });
     }
+}
+
+function changeMode() {
+    if(draw) draw = false;
+    else if(!draw) draw = true;
 }
